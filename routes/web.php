651f90a81f05a0b1920b3c588ef8da992ef7d62c
kyrufa <kyrufa2@gmail.com>
2025-04-25 11:03:26 +0700

    login dan dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,75 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
+// Route untuk halaman login
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register'])->name('register.post');
+
+// Route untuk halaman welcome (jika masih diperlukan)
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/aspirasi', function () {
+    return view('aspirasi');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/petugas', function () {
+    return view('petugas');
+});
+
+
+
+// Route untuk halaman setelah login
+Route::get('/home', function () {
+    return view('home');
+})->middleware('auth');
+
+/*
+
+<form class="user">
+                                        <div class="form-group">
+                                            <input type="email" class="form-control form-control-user"
+                                                id="exampleInputEmail" aria-describedby="emailHelp"
+                                                placeholder="Enter Email Address...">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user"
+                                                id="exampleInputPassword" placeholder="Password">
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="custom-control custom-checkbox small">
+                                                <input type="checkbox" class="custom-control-input" id="customCheck">
+                                                <label class="custom-control-label" for="customCheck">Remember
+                                                    Me</label>
+                                            </div>
+                                        </div>
+                                        <a href="/home" class="btn btn-primary btn-user btn-block">
+                                            Login
+                                        </a>
+                                        <hr>
+                                        <a href="/home" class="btn btn-google btn-user btn-block">
+                                            <i class="fab fa-google fa-fw"></i> Login with Google
+                                        </a>
+                                        <a href="/home" class="btn btn-facebook btn-user btn-block">
+                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                        </a>
+                                    </form>
+
+
+*/
